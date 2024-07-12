305711fa1fd5774d29976dd38e7dccefe1039157
Veeles <kacper.ros2@interia.pl>
2024-07-12 18:02:18 +0200

    Create static file and create nav in header.html

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static 
 from django.http import HttpResponse
 from django.urls import path
 from . import views
@@ -24,9 +25,9 @@ import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home_page'),
+    path('', views.home_view, name='home_page',),
     path('test/', views.test_view),
 
-]
+] + static(settings.MEDIA_URL)
 
 

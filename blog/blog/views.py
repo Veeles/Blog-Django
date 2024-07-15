469d69ade47dfe5cd5e4161408cbb9b96144e28f
Veeles <kacper.ros2@interia.pl>
2024-07-15 13:15:13 +0200

    Return data from view to index.html

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -3,8 +3,15 @@ from django.template import loader, TemplateDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
 import os
+from posts.models import Post
+
 def home_view(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all().values()
+    content = {'mydata': posts}
+    template = loader.get_template('index.html')
+    # return render(request, 'index.html')
+    print(content)
+    return HttpResponse(template.render(content, request))
 
 
 def login(request):

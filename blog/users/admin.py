df18d84c46ea7b38b8c491fbf70bc6e7d8d75fc3
Veeles <kacper.ros2@interia.pl>
2024-07-15 12:54:08 +0200

    Update card in index.html code

--- blog/users/admin.py
+++ blog/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from posts.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'photo', 'author')
+
+admin.site.register(Post, PostAdmin)
